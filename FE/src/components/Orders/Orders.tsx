e6dd7cc76829431ca7027ec8fd4794248fc79d54
Matheus Engel <102177203+engelzz@users.noreply.github.com>
2024-07-14 20:07:07 -0300

    feat - websockets implementation

--- FE/src/components/Orders/Orders.tsx
+++ FE/src/components/Orders/Orders.tsx
@@ -1,23 +1,69 @@
+import socketIo from 'socket.io-client';
+
+import { useEffect, useState } from "react";
+import { Order } from "../../types/Order.ts";
+import { api } from "../../utils/api.ts";
+import { OrderBoard } from "../OrderBoard/OrderBoard.tsx";
 import { Container } from "./styles.ts";
-import {OrderBoard} from "../OrderBoard/OrderBoard.tsx";
 
 export default function Orders() {
+  const [orders, setOrders] = useState<Order[]>([]);
+
+  useEffect (() => {
+    const socket = socketIo('http://localhost:3001', {
+      transports: ['websocket'],
+    });
+
+    socket.on('orders@new', (order) => {
+      setOrders(prevState => prevState.concat(order))
+    })
+  }, []);
+
+  useEffect(() => {
+    api.get('/orders')
+      .then(({ data }) => {
+        setOrders(data)
+      })
+  }, []);
+
+  const waiting = orders.filter((order) => order.status === 'WAITING');
+  const done = orders.filter((order) => order.status === 'DONE');
+  const inProduction = orders.filter((order) => order.status === 'IN_PRODUCTION');
+
+  function handleCancelOrder(orderId: string) {
+    setOrders((prevState) => prevState.filter(order => order._id !== orderId))
+  }
+
+  function handleOrderStatusChange(orderId: string, status: Order['status']) {
+    setOrders((prevState) => prevState.map((order) => (
+      order._id === orderId
+      ? { ...order, status }
+      : order
+    )))
+  }
+
   return (
     <Container>
      <OrderBoard
        icon="🕒"
        title="Fila de espera"
-       orders={[]}
+       orders={waiting}
+       onCancelOrder={handleCancelOrder}
+       onChangeOrderStatus={handleOrderStatusChange}
      />
      <OrderBoard
        icon="👨‍🍳"
        title="Em preparação"
-       orders={[]}
+       orders={inProduction}
+       onCancelOrder={handleCancelOrder}
+       onChangeOrderStatus={handleOrderStatusChange}
      />
      <OrderBoard
        icon="✅"
        title="Pronto"
-       orders={[]}
+       orders={done}
+       onCancelOrder={handleCancelOrder}
+       onChangeOrderStatus={handleOrderStatusChange}
      />
     </Container>
   )
