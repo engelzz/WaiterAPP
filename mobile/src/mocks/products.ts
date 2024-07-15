9823e67fd7709b079a3a070abe4608924ec6e10a
Matheus Engel <102177203+engelzz@users.noreply.github.com>
2024-07-15 18:24:23 -0300

    feat: products and categories mocks for dev

---
+++ mobile/src/mocks/products.ts
@@ -0,0 +1,34 @@
+export const products = [
+  {
+		_id: "6686b726455614ef9f3b9029",
+		name: "Pizza Quatro quejios",
+		description: "Pizza com borda top cheio de queijo kkakakaa",
+		imagePath: "1720105991286-quatro-queijos.png",
+		price: 40,
+		ingredients: [
+			{
+				name: "Mussarela",
+				icon: "🧀",
+				_id: "6686b726455614ef9f3b902a"
+			},
+			{
+				name: "Parmesão",
+				icon: "🧀",
+				_id: "6686b726455614ef9f3b902b"
+			},
+			{
+				name: "Brie",
+				icon: "🧀",
+				_id: "6686b726455614ef9f3b902c"
+			}
+		],
+	},
+  {
+    _id: '6372e276a381106c0f854cb3',
+    name: 'Coca cola',
+    description: 'Coca cola lata geladinha topzera',
+    imagePath: '1720106144937-coca-cola.png',
+    price: 7,
+    ingredients: [],
+  },
+];
